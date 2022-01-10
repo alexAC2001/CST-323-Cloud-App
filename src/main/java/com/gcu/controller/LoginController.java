b646b7a0aec7deee6d7168c9b07101f3ccafe966
alexAC2001 <alex.a.carrillo2001@gmail.com>
2022-01-09 19:34:41 -0700

    Sat Push 2
    Using CST-339 - User can go from Login to view Orders

--- src/main/java/com/gcu/controller/LoginController.java
+++ src/main/java/com/gcu/controller/LoginController.java
@@ -1,5 +1,8 @@
 package com.gcu.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.gcu.model.LoginModel;
+import com.gcu.model.OrderModel;
 
 @Controller
 @RequestMapping("/login")
@@ -25,10 +29,20 @@ public class LoginController
 	@PostMapping("/doLogin")
 	public String doLogin(LoginModel loginModel, BindingResult bindingResult, Model model)
 	{
-		System.out.println(String.format("Form with Username of % and a Password of %", loginModel.getUsername(), loginModel.getPassword()));
+		System.out.println(String.format("Form with Username of %s and Password of %s", loginModel.getUsername(), loginModel.getPassword()) );
+		
+		List<OrderModel> orders = new ArrayList<OrderModel>();
+		orders.add(new OrderModel(0L, "0001", "Product 1", 1.00f, 1));
+		orders.add(new OrderModel(1L, "0002", "Product 2", 2.00f, 2));
+		orders.add(new OrderModel(2L, "0003", "Product 3", 3.00f, 3));
+		orders.add(new OrderModel(3L, "0004", "Product 4", 4.00f, 4));
+		orders.add(new OrderModel(4L, "0005", "Product 5", 5.00f, 5));
+		
+		model.addAttribute("title", "Order Form");
+		model.addAttribute("orders", orders);
 		
 		// Navigate back to the Login view
-		return "login";
+		return "orders";
 	}
 
 }
